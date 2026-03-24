1f819c2abbea68cde8ef4b0fdca90d65d04f8ceb
Josué Luís Pimenta Alecrim <JosueAlecrim7@gmail.com>
2026-03-24 20:26:02 -0300

    Add files via upload

---
+++ Estruturas de repetição/src/App.java
@@ -0,0 +1,18 @@
+// NOME: Josue Luis Pimenta Alecrim
+// MATRICULA: 1261948087
+import java.util.Scanner;
+public class App {
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número inteiro:");
+        int x = entrada.nextInt();
+        int resultado = x / 2;
+        if (x % 2 == 0) {
+        System.out.println("é um numero par");
+    } else {
+        System.out.println("é um numero impar");
+        }
+    }
+   }
+  
+
